ddfaa1b20eecd46571c6d7d5ea3a81c5d2afe4d2
Ben Chatelain <ben@octop.ad>
2022-09-11 17:31:46 -0600

    ➕ Ditto (2.0.5)

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
             targets: ["DittoStorageEngine"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mergesort/Bodega.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/getditto/DittoSwiftPackage.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/mergesort/Bodega.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .target(
